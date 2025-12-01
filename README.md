86c72cb8cc6501df679ff78ea0d6753c365abba0
rullo16 <rullofederico16@gmail.com>
2025-12-01 17:01:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MonsterWorld_1
+
+Developed with Unreal Engine 5
---
+++ Source/MonsterWorld_1/MonsterWorld_1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MonsterWorld_1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MonsterWorld_1, "MonsterWorld_1" );
